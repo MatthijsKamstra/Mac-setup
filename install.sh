0f58482adae1a52f7e8026b58de81efb6cd83e3d
Matthijs Kamstra <matthijs@fonk.amsterdam>
2022-09-30 16:09:33 +0200

    add new installs

--- install.sh
+++ install.sh
@@ -17,39 +17,83 @@
 # sh android.sh ## not doing android anymore
 
 
+# # start from zero
+# sh _clean.sh
+# sh _osxprep.sh
+
+# # beat osx into shape
+# sh _macos.sh
+
+# # install programmers fonts
+# sh _font.sh
+
+# # install all via brew
+# sh _brew_cask.sh
+# sh _brew_list.sh
+# sh _brew.sh
+
+# # install global npm
+# sh _npm.sh
+
+# # creative
+# sh _creativecoding.sh
+# sh _design.sh
+
+# # misc
+# # sh _haxe.sh # not for now
+# sh _socials.sh
+# sh _web.sh
+# sh _misc.sh
+
+# sh _pomodore.sh
+# sh _screencapture.sh
+
+# # run `sh _backup_settings.sh` if you want to backup some
+# # sh _backup_settings.sh
+# # sh _list_program.sh
+
+
 # start from zero
 sh _clean.sh
 sh _osxprep.sh
-
 # beat osx into shape
 sh _macos.sh
+
+# iterm
+sh _ohmyzsh.sh
 
 # install programmers fonts
 sh _font.sh
 
-# install all via brew
+# brew
+sh _brew.sh
 sh _brew_cask.sh
 sh _brew_list.sh
-sh _brew.sh
 
 # install global npm
 sh _npm.sh
+
+# misc
+sh _web.sh
+sh _socials.sh
+sh _misc.sh
 
 # creative
 sh _creativecoding.sh
 sh _design.sh
 
-# misc
-# sh _haxe.sh # not for now
-sh _socials.sh
-sh _web.sh
-sh _misc.sh
-
+# other stuff
 sh _pomodore.sh
 sh _screencapture.sh
 
-# run `sh _backup_settings.sh` if you want to backup some
-# sh _backup_settings.sh
-# sh _list_program.sh
+
+# Haxe
+# _haxe.sh
+# _haxelib.sh
+# _backup_settings.sh
+
+
+# _list_program.sh
+
 
 
